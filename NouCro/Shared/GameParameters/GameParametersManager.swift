5f880408e3c863d5663b6af0626cbabb5c644cf7
alireza <alireza.alemi76@icloud.com>
2024-10-28 20:35:15 -0600

    added a new module to manage game states separately.

---
+++ NouCro/Shared/GameParameters/GameParametersManager.swift
@@ -0,0 +1,65 @@
+//
+//  GameParametersManager.swift
+//  NouCro
+//
+//  Created by AliReza on 2024-10-28.
+//
+
+import Combine
+
+class GameParametersManager: GameParametersProvider {
+    
+    static let shared = GameParametersManager()
+    private var cancellables: Set<AnyCancellable> = []
+    
+    private init() {}
+    
+    func getPlayers(onCompletion: @escaping ([Player]) -> Void) {
+        PersistenceManager.shared.get().sink { [weak self] result in
+            switch result {
+            case .finished:
+                break
+            case .failure(_):
+                print("Default values")
+                let players = self?.getDefaultPlayers() ?? []
+                onCompletion(players)
+            }
+        } receiveValue: { (players: [Player]) in
+            print("Previous values")
+            onCompletion(players)
+        }.store(in: &cancellables)
+        
+    }
+    
+    func getGridSize(onCompletion: @escaping (Int) -> Void) {
+        PersistenceManager.shared.get().sink { [weak self] result in
+            switch result {
+            case .finished:
+                break
+            case .failure(_):
+                print("Default values")
+                let size = self?.getDefaultGridSize()
+                onCompletion(Int(size?.size ?? 0))
+            }
+        } receiveValue: { (size: [GridSize]) in
+            print("Previous values")
+            onCompletion(Int(size.first?.size ?? 0))
+        }.store(in: &cancellables)
+        
+    }
+    
+    private func getDefaultPlayers() -> [Player] {
+        let players =  [
+            Player(name: "Annie", color: "#663DFF", icon: "xmark"),
+            Player(name: "Alex", color: "#CC4499", icon: "circle")
+        ]
+        PersistenceManager.shared.save()
+        return players
+    }
+    
+    private func getDefaultGridSize() -> GridSize {
+        let size = GridSize(size: 3)
+        PersistenceManager.shared.save()
+        return size
+    }
+}
---
+++ NouCro/Shared/GameParameters/GameParametersProvider.swift
@@ -0,0 +1,13 @@
+//
+//  GameParametersProvider.swift
+//  NouCro
+//
+//  Created by AliReza on 2024-10-28.
+//
+
+import Foundation
+
+protocol GameParametersProvider {
+    func getPlayers(onCompletion: @escaping ([Player]) -> Void )
+    func getGridSize(onCompletion: @escaping (Int) -> Void)
+}
